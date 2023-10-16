ed876d32f56a37370f3fb495c8ccb907002cfff2
Priyank Sinha <priyank.sinha14@gmail.com>
2023-10-16 15:38:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,10 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+//app.use(cors());
+app.use(cors({
+  origin: 'https://652d083d5b675c27bf9c20ed--dazzling-syrniki-a8e541.netlify.app'
+}));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
