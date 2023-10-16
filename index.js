1cb5986b5f3a6bfe71e0e7b3ea5772c3f225cfdb
Priyank Sinha <priyank.sinha14@gmail.com>
2023-10-16 18:35:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,14 +30,12 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 //app.use(cors());
-app.use(cors({
-  origin: 'https://652d083d5b675c27bf9c20ed--dazzling-syrniki-a8e541.netlify.app'
-}));
-app.use(function (req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
-})
+app.use(cors());
+// app.use(function (req, res, next) {
+//    res.header("Access-Control-Allow-Origin", "*");
+//    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//    next();
+// })
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
